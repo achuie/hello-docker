ea455cb9d2f2c1ca37df42615f0121481c115b0e
Andrew Huie <andrew@ascent.ai>
2020-11-10 15:47:25 +0900

    Video works; no audio.

--- syncplay/start_container.sh
+++ syncplay/start_container.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 
 docker run --rm -it --name syncplay \
-    -u $(id -u ${USER}):$(id -g ${USER}) \
     --hostname syncplay \
+    --ipc=host \
     -v /etc/shadow:/etc/shadow:ro \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
-    -v /home/achuie:/home/achuie \
-    -v /tmp/.X11-unix/X1:/tmp/.X11-unix/X1 \
-    -e DISPLAY=:1 \
+    -v /tmp/.X11-unix/X1:/tmp/.X11-unix/X1:rw \
+    -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     syncplay
